423d379d66a873fa1c17de52622f86685cd83cfa
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-03-08 13:11:46 -0500

    Added penalty to late submissions

--- index.js
+++ index.js
@@ -76,95 +76,105 @@ const LearnerSubmissions = [
   },
 ];
 
-function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
+// Get the leaners id
+function getLearnersID(submissions) {
+  // Use map to iterate the submissions array then return all learn_id
+  // I used set in order not to get duplicate learner_id
+  let learnerIDSet = new Set(
+    submissions.map((submission) => submission.learner_id)
+  );
 
-  // Get the leaners id
-  function getLearnersID(submissions) {
-    // Use map to iterate the submissions array then return all learn_id
-    // I used set in order not to get duplicate learner_id
-    let learnerIDSet = new Set(
-      submissions.map((submission) => submission.learner_id)
-    );
+  //Convert learnerIDSet into an array
+  let learnerID = [];
+  learnerIDSet.forEach((id) => learnerID.push(id));
 
-    //Convert learnerIDSet into an array
-    let learnerID = [];
-    learnerIDSet.forEach((id) => learnerID.push(id));
+  return learnerID;
+}
 
-    return learnerID;
-  }
-  // console.log(getLearnersID(submissions));
+// Get the assignments that are due
+function getAssignmentsDue(ag) {
+  let currentDate = new Date();
+  // Filter the Assignment group assignment objects and return assignments that are due
+  return ag.assignments.filter((assignment) => {
+    // Used  new Date String because right now it is a string , so I need it to match my current date variable data type which is an object in order to compare it
+    return new Date(assignment.due_at) < currentDate;
+  });
+}
 
-  // Get the assignments that are due
-  function getAssignmentsDue(ag) {
-    let currentDate = new Date();
-    // Filter the Assignment group assignment objects and return assignments that are due
-    return ag.assignments.filter((assignment) => {
-      // Used  new Date String because right now it is a string , so I need it to match my current date variable data type which is an object in order to compare it
-      return new Date(assignment.due_at) < currentDate;
-    });
-  }
-  //   console.log(getAssignmentsDue(ag));
+//get score by Id
+function getScore(submissions, ag) {
+  let leanersID = getLearnersID(submissions);
+  let assignments = getAssignmentsDue(ag);
+  let totalScore = 0;
+  let assignmentScores = [];
+  let scores = [];
 
-  //get score by Id
-  function getScore(submissions, ag) {
-    let leanersID = getLearnersID(submissions);
-    let assignments = getAssignmentsDue(ag);
-    let totalScore = 0;
-    let assignmentScores = [];
-    let scores = [];
-
-    // Iterate through each learner, submission, assignments due
-    leanersID.forEach((leaner) => {
-      for (let i = 0; i < submissions.length; i++) {
-        for (let j = 0; j < assignments.length; j++) {
-          //match the submission assignment id with assignments due id to only get assignments that are due
-          if (submissions[i].assignment_id === assignments[j].id) {
-            if (leaner === submissions[i].learner_id) {
+  // Iterate through each learner, submission, assignments due
+  leanersID.forEach((leaner) => {
+    for (let i = 0; i < submissions.length; i++) {
+      for (let j = 0; j < assignments.length; j++) {
+        //match the submission assignment id with assignments due id to only get assignments that are due
+        if (submissions[i].assignment_id === assignments[j].id) {
+          if (leaner === submissions[i].learner_id) {
+            if (
+              submissions[i].submission.submitted_at > assignments[j].due_at
+            ) {
+              let penalty = 0;
+              penalty = (10 * assignments[j].points_possible) / 100;
+              totalScore += submissions[i].submission.score - penalty;
+            } else {
               //calculate the total score earned for learner
               totalScore += submissions[i].submission.score;
-              // Add assignent 1 and assignment 2 score to assignmentsScores Array
-              assignmentScores.push(submissions[i].submission.score);
             }
+            // Add assignent 1 and assignment 2 score to assignmentsScores Array
+            assignmentScores.push(submissions[i].submission.score);
           }
         }
       }
-      scores.push(totalScore);
-      totalScore = 0;
-    });
-    return scores;
-  }
-  //   getScore(submissions, ag);
-
-  // Get total possible points
-  function getTotalPossiblePoints(ag) {
-    // Declare an array to save the return array from only the assignments due
-    let assignments = getAssignmentsDue(ag);
-    // Use reduce to get the total amount of points by adding all points_possible from due assignments
-    return assignments.reduce(
-      (total, assignment) => (total += assignment.points_possible),
-      0
-    );
-  }
-  // console.log(getTotalPossiblePoints(ag));
-
-  //Get weighted Average
-  function getWeightedAverage(ag, submissions) {
-    let learnerId = getLearnersID(submissions);
-    let totalPointsPossible = getTotalPossiblePoints(ag);
-    let totalScore = getScore(submissions, ag);
-    let learnerAverage = 0;
-    let average = [];
-
-    for (let i = 0; i < totalScore.length; i++) {
-      learnerAverage = totalScore[i] / totalPointsPossible;
-      average.push(learnerAverage);
-      learnerAverage = 0;
     }
+    scores.push(totalScore);
+    totalScore = 0;
+  });
+  return scores;
+}
 
-    return average;
+// Get total possible points
+function getTotalPossiblePoints(ag) {
+  // Declare an array to save the return array from only the assignments due
+  let assignments = getAssignmentsDue(ag);
+  // Use reduce to get the total amount of points by adding all points_possible from due assignments
+  return assignments.reduce(
+    (total, assignment) => (total += assignment.points_possible),
+    0
+  );
+}
+
+//Get weighted Average
+function getWeightedAverage(ag, submissions) {
+  let learnerId = getLearnersID(submissions);
+  let totalPointsPossible = getTotalPossiblePoints(ag);
+  let totalScore = getScore(submissions, ag);
+  let learnerAverage = 0;
+  let average = [];
+
+  for (let i = 0; i < totalScore.length; i++) {
+    learnerAverage = totalScore[i] / totalPointsPossible;
+    average.push(learnerAverage);
+    learnerAverage = 0;
   }
-//   console.log(getWeightedAverage(ag, submissions));
+
+  return average;
+}
+
+function getLearnerData(course, ag, submissions) {
+  // here, we would process this data to achieve the desired result.
+
+  // console.log(getLearnersID(submissions));
+  // console.log(getAssignmentsDue(ag));
+  // console.log(getScore(submissions, ag));
+  // console.log(getTotalPossiblePoints(ag));
+  // console.log(getWeightedAverage(ag, submissions));
+  
 
   let learnersIDs = getLearnersID(submissions);
   const result = [
@@ -186,7 +196,7 @@ function getLearnerData(course, ag, submissions) {
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
-console.log(result);
+// console.log(result);
 // console.log(AssignmentGroup.assignments);
 
 const desiredResult = [
